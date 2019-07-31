d60f3ea8c1ba29ca3f7b3895726e27e0e6c5ab3b
jalondono <juanlondono151776@hotmail.com>
2019-07-31 00:00:38 +0000

    select and formart and replace de format with a corresponding value , the print

---
+++ select_format.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ *select_format - program thisgs
+ *Result: always return 0
+ *@args: variable de entrada
+ *@format : formato of string
+ *Return: value depending on function
+ */
+int select_format(char format, va_list args)
+{
+	char *result;
+	int size = 0;
+	int numero = 0;
+
+	switch (format)
+	{
+	case 'c':
+		_putchar(va_arg(args, int));
+		size = 1;
+		break;
+	case 's':
+		result = print_string(args);
+		if (result == NULL)
+		{
+			_puts("(null)"), size = 6;
+			break;
+		}
+		_puts(result);
+		size = _strlen(result);
+		break;
+	case '%':
+		_putchar('%'), size = 1;
+		break;
+	case 'd': case 'i':
+		numero = va_arg(args, int);
+		if (numero == 0)
+		{
+			_putchar('0'), size = 1;
+			break;
+		}
+		size = print_integer_d(numero);
+		break;
+	case 'x': case 'X': case 'o': case 'u':
+		size = convert(va_arg(args, unsigned int), format);
+		break;
+	default:
+		_putchar('%'), _putchar(format), size = 2;
+		break;
+	}
+	return (size);
+}
